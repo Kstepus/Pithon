2c6c0265bd0d906a12555ff0e7dd1351e71681a9
Kstepus <Kstepus333@gmail.com>
2024-11-15 21:36:52 +0800

    module3_1.py

--- module3_1.py
+++ module3_1.py
@@ -10,6 +10,6 @@ def  is_contains(string, list_to_search):
     return string.upper() in [s.upper() for s in list_to_search]
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) 
+print(is_contains('cycle', ['recycling', 'cyclic']))  
 print(calls)
