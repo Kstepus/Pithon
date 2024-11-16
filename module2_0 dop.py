7b243b52c9e526b5999993ecc5430295ef224f5e
Kstepus <Kstepus333@gmail.com>
2024-11-16 21:33:36 +0800

    module2_0 hard.py
    дополнительное задание по модулю_2

--- module2_0 dop.py
+++ module2_0 dop.py
@@ -1,14 +1,14 @@
-def find_pairs (n):
-    result = []
-    for i in range(1, n):
-        for j in range(i + 1, n + 1):
-            pair_sum = i + j
-            if pair_sum != 0 and n % pair_sum == 0:
-                result_append(f"({1} + {j})")
-                return ','.join(result)
-            n = int(input("Введите число n (от 3 до 20): "))
-            if 3 <= n <= 20:
-                password = find_pairs (n)
-                print("найденные пары", password )
-            else:
-                print("Число должно быть в диапазоне от 3 до 20.")
+n = int(input('Введите целое число от 3 до 20: '))
+def get_password(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+
+result = get_password(n)
+print('Пароль:', result)
